3baa86d47d35960c0df7e6996b3aaee84fbbf4b2
Clayton Phillips-Dorsett <claytonphillipsdorsett@gmail.com>
2018-01-27 02:30:55 -0500

    Fixed ordered json response and formatted user navigation

--- pythonmodels/static/pythonmodels/js/landing.js
+++ pythonmodels/static/pythonmodels/js/landing.js
@@ -2,12 +2,22 @@
 $(document).ready(function() {
 
     // Set active navigation li
-    var activeTab = $("a[href='" + window.location.pathname + "']").parent();
+    // Change path if active li is create model tab
+    var path = window.location.pathname;
+        if (path.match(/create/i)) {
+            path = path.replace(/\d+$/, "0")
+        }
+    var activeTab = $("a[href='" + path + "']").parent();
     activeTab.addClass('active');
 
     // Hide collapsed navigation after click
     $('.nav a').click(function(){
         $('#myNavbar').collapse('hide');
     });
+
+    var $lh = $('.landing').height();
+    var $hh = $('.header').height();
+    var $fh = $('footer').height();
+    $('.landing').height($lh - $hh - $fh - 30);
 
 });
