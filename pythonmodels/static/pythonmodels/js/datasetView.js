b88d48d23a50389b81f1d2ea18a282e4ba3979a7
Clayton Phillips-Dorsett <claytonphillipsdorsett@gmail.com>
2018-02-12 20:10:32 -0500

    Added spinner while var info table loads

--- pythonmodels/static/pythonmodels/js/datasetView.js
+++ pythonmodels/static/pythonmodels/js/datasetView.js
@@ -40,9 +40,11 @@ $(document).ready(function () {
 
     /**
      * Create datatables and add back opacity to show them
+     * Remove spinner once datables are loaded
      */
     $('#varNumTable, #varOtherTable').DataTable({'dom': 'ftip'});
     $('#varTables').css('opacity', 1);
+    $('.varTableSpinner').hide();
 
 
     /**
